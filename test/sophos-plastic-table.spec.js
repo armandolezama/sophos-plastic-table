c7b83e5a34547412eef21cbd9bca2469393fe369
armando <armando.lezama.psic@gmail.com>
2021-11-25 01:48:26 -0600

    chore(*):initial commit and table basic functionality

---
+++ test/sophos-plastic-table.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+import { fixture, assert } from "@open-wc/testing";
+import '../sophos-plastic-table';
+
+describe('Suite cases', () => {
+  it('First test', () => {
+    const _element = await fixture('<sophos-plastic-table><sophos-plastic-table/>');
+  });
+});
